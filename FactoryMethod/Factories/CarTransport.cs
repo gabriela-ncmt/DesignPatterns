1881eb57ff572728b6a2f1f2e6ff10b79dfa9d71
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-03-13 12:48:27 -0300

    fix: removendo importações não utilizadas

--- FactoryMethod/Factories/CarTransport.cs
+++ FactoryMethod/Factories/CarTransport.cs
@@ -1,9 +1,4 @@
 ﻿using FactoryMethod.Vehicle;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FactoryMethod.Factories
 {
