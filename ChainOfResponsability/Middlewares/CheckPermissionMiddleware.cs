92252797a8cb83ba55371e2a7388a237695bb6d6
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-05-22 21:10:38 -0300

    feat: implementação Chain Of Responsability

---
+++ ChainOfResponsability/Middlewares/CheckPermissionMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace ChainOfResponsability.Middlewares
+{
+    public class CheckPermissionMiddleware : Middleware
+    {
+        public override bool Check(string email, string password)
+        {
+            if (email.Equals("master@teste.com"))
+            {
+                Console.WriteLine("Seja bem vindo administrador!");
+                return true;
+            }
+
+            Console.WriteLine("Seja bem vindo!)");
+
+            return CheckNext(email, password);
+        }
+    }
+}
---
+++ ChainOfResponsability/Program.cs
@@ -0,0 +1,40 @@
+﻿using ChainOfResponsability.Middlewares;
+using ChainOfResponsability.Servers;
+
+namespace ChainOfResponsability
+{
+    class Program
+    {
+        private static Server server;
+        static void Init()
+        {
+            server = new Server();
+
+            server.RegisterUser("master@teste.com", "master@342341w#as");
+            server.RegisterUser("user@teste.com", "user234235");
+
+            Middleware middleware = new CheckUserMiddleware(server);
+
+            middleware.LinkWith(new CheckPermissionMiddleware());
+
+            server.SetMiddleware(middleware);
+        }
+        static void Main(string[] args) 
+        { 
+            Init();
+
+            Boolean done = false;
+
+            do
+            {
+                Console.WriteLine("Digite o seu e-mail: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("Digite a senha: ");
+                string password = Console.ReadLine();
+
+                done = server.Login(email, password);
+            } while (!done);
+            Console.ReadLine();
+        }
+    }
+}
